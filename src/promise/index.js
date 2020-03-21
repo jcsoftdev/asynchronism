bd7252c54594cc38e3f508390da3ebe0ef4fae3c
jcsoftia <ajcarlos032@gmail.com>
2020-03-21 14:30:40 -0500

    promise

---
+++ src/promise/index.js
@@ -0,0 +1,35 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if(true){
+      resolve('Hey!')
+    } else {
+      reject('Wooops!')
+    }
+  })
+}
+
+somethingWillHappen()
+  .then(response => console.log(response))
+  .catch(err => console.error(err))
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(()=>{
+        resolve('True')
+      },2000)
+    } else {
+      const error = new Error('Wooops!')
+      reject(error)
+    }
+  })
+}
+
+somethingWillHappen2()
+  .then(response => console.log(response))
+  .then(()=>{console.log('Hello')})
+  .catch(error => console.error(error))
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(response => console.log('Array of results', response))
+  .catch(error => console.log(error))
